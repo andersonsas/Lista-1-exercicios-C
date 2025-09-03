67fe5e291024aa5291b5552328a5542080469580
anderson <anderson_under@hotmail.com>
2025-09-03 12:35:39 -0400

    Iniciando Lista Dois

---
+++ 02_Estrutura_de_Selecao/Exercicio-01.c
@@ -0,0 +1,5 @@
+/*
+    Faça um programa que receba quatro notas de um aluno, calcule e imprima a média aritmética
+    das notas e a mensagem de aprovado para média superior ou igual a 7.0 ou a mensagem de
+    reprovado para média inferior a 7.0.
+*/
